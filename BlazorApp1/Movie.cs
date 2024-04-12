9a20f11bf90456f1fb2e271ac2d6143b02370344
GLRandula <lakithrandula28@gmail.com>
2024-04-12 15:03:15 +0530

    Add user input validations.

--- BlazorApp1/Movie.cs
+++ BlazorApp1/Movie.cs
@@ -1,15 +1,20 @@
-﻿namespace BlazorApp1
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1
 {
     public class Movie
     {
 
         public int Id { get; set; }
+
+        [Required]
         public string? Title { get; set; }
 
         public DateTime? ReleaseDate { get; set; }
 
         public string? Genre { get; set; }
 
+        [Range(0, 100)]
         public decimal? Price { get; set; }
     }
 }
